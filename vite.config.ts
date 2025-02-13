a8a8f3a2c1cba24dc7266777f740b440ec825003
titulus <tituilus.web@gmail.com>
2025-02-13 21:11:10 +0300

    Configure Vite build options for output directory and file naming

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,20 @@ import { defineConfig } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
+  base: '',
   plugins: [
     createHtmlPlugin({
       minify: true,
     }),
   ],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        assetFileNames: '[name][extname]',
+        chunkFileNames: '[name].js',
+        entryFileNames: '[name].js',
+      },
+    },
+  },
 });
