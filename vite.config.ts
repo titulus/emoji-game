17f732c87d0e821746ce1f65322be4f5f25d801e
titulus <tituilus.web@gmail.com>
2025-02-13 23:47:02 +0300

    fix images copy and build

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
 import { defineConfig } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   base: '',
   plugins: [
-    createHtmlPlugin({
-      minify: true,
-    }),
-  ],
+        createHtmlPlugin({
+          minify: true,
+        }),
+        viteStaticCopy({
+          targets: [
+            {
+              src: 'icons',
+              dest: '',
+            },
+            ...(process.env.INCLUDE_SDK === 'true' ? [{
+              src: 'sdk.js',
+              dest: '',
+            }] : []),
+          ],
+        }),
+      ],
   build: {
     outDir: 'dist',
     rollupOptions: {
